6f379ae860b7c0bb5a2c587da04255d7f3ecdfc6
Vimal Patel <vimalm@iisc.ac.in>
2021-10-22 09:40:09 +0530

    DRAM to DRAM stats on Chisel-3
    all sockets are in fsdax mode

--- microbenchmarks/run_all.sh
+++ microbenchmarks/run_all.sh
@@ -1,23 +1,23 @@
 sudo sysctl kernel.numa_balancing=0
-# # run concurrent page migration benchmarks
-# cd concurrent_page_migration;
-# make non_thp_move_pages;
-# make thp_move_pages;
-# echo "============executing concurrent_page_migration/run_non_thp_test.sh================"
-# ./run_non_thp_test.sh
-# echo "============executing concurrent_page_migration/run_thp_test.sh===================="
-# ./run_thp_test.sh
-# cd ..
+# run concurrent page migration benchmarks
+cd concurrent_page_migration;
+make non_thp_move_pages;
+make thp_move_pages;
+echo "============executing concurrent_page_migration/run_non_thp_test.sh================"
+./run_non_thp_test.sh
+echo "============executing concurrent_page_migration/run_thp_test.sh===================="
+./run_thp_test.sh
+cd ..
 
-# # run exchange page migration benchmark
-# cd exchange_page_migration
-# make non_thp_move_pages;
-# make thp_move_pages;
-# echo "============executing exchange_page_migration/run_non_thp_test.sh================"
-# ./run_non_thp_test.sh
-# echo "============executing exchange_page_migration/run_thp_test.sh===================="
-# ./run_thp_test.sh
-# cd ..
+# run exchange page migration benchmark
+cd exchange_page_migration
+make non_thp_move_pages;
+make thp_move_pages;
+echo "============executing exchange_page_migration/run_non_thp_test.sh================"
+./run_non_thp_test.sh
+echo "============executing exchange_page_migration/run_thp_test.sh===================="
+./run_thp_test.sh
+cd ..
 
 # run parallel and native thp page migration benchmark
 cd thp_page_migration_and_parallel
