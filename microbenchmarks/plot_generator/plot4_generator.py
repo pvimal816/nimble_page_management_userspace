ba824425f3e2a2fa59650b56bf165e63429c2059
Vimal Patel <vimalm@iisc.ac.in>
2021-11-07 21:09:10 +0530

    Chisel-3 DRAM to DRAM performance stats

--- microbenchmarks/plot_generator/plot4_generator.py
+++ microbenchmarks/plot_generator/plot4_generator.py
@@ -18,18 +18,18 @@ def main():
     ]
 
     # mt=4, non-concurrent, 4KB pages
-    four_thread_non_concurrent_base_page = [
-        (1<<i)*1e9/((1<<18)*stats("../thp_page_migration_and_parallel"
+    four_thread_concur_base_page = [
+        (1<<i)*1e9/((1<<18)*stats("../concurrent_page_migration"
             + "/stats_4kb"
-            + "/mt_4_4kb_page_order_{0}".format(i)).
+            + "/mt_4_page_order_{0}_batch".format(i)).
         average_stats["Total_cycles"]) for i in page_count
     ]
     
     # mt=4 concurrent, 4KB pages
-    four_thread_concurrent_base_page = [
-        (1<<i)*1e9/((1<<18)*stats("../concurrent_page_migration"
+    four_thread_concur_exchange_base_page = [
+        (1<<i)*1e9/((1<<18)*stats("../exchange_page_migration"
             + "/stats_4kb"
-            + "/mt_4_page_order_{0}_batch".format(i)).
+            + "/mt_4_page_order_{0}_exchange_batch".format(i)).
         average_stats["Total_cycles"]) for i in page_count
     ]
     
@@ -41,20 +41,19 @@ def main():
         average_stats["Total_cycles"]) for i in page_count
     ]
 
-
     # mt=4, non-concurrent, THP pages
-    four_thread_non_concurrent_thp_page = [
-        (1<<i)*1e9/((1<<9)*stats("../thp_page_migration_and_parallel"
-            + "/stats_thp"
-            + "/mt_4_2mb_page_order_{0}".format(i)).
+    four_thread_concur_thp_page = [
+        (1<<i)*1e9/((1<<9)*stats("../concurrent_page_migration"
+            + "/stats_2mb"
+            + "/mt_4_page_order_{0}_batch".format(i)).
         average_stats["Total_cycles"]) for i in page_count
     ]
 
     # mt=4, concurrent, THP pages
-    four_thread_concurrent_thp_page = [
-        (1<<i)*1e9/((1<<9)*stats("../concurrent_page_migration"
+    four_thread_concur_exchange_thp_page = [
+        (1<<i)*1e9/((1<<9)*stats("../exchange_page_migration"
             + "/stats_2mb"
-            + "/mt_4_page_order_{0}_batch".format(i)).
+            + "/mt_4_page_order_{0}_exchange_batch".format(i)).
         average_stats["Total_cycles"]) for i in page_count
     ]
 
@@ -64,8 +63,8 @@ def main():
         "migration_type": []
     }
 
-    thp_y_data = [thp_migration, four_thread_non_concurrent_thp_page, four_thread_concurrent_thp_page]
-    thp_labels = ["THP Migration", "4-Thread Non-concurrent", "4-Thread Concurrent"]
+    thp_y_data = [thp_migration, four_thread_concur_thp_page, four_thread_concur_exchange_thp_page]
+    thp_labels = ["THP Migration", "4-Thread Concur Migrate", "4-Thread Concur Exchange"]
 
     for j in range(0, len(thp_y_data)):
         for i in page_count:
@@ -79,8 +78,8 @@ def main():
         "migration_type": []
     }
 
-    base_y_data = [base_page_migration, four_thread_non_concurrent_base_page, four_thread_concurrent_base_page]
-    base_page_labels = ["Base Page Migration", "4-Thread Non-concurrent", "4-Thread Concurrent"]
+    base_y_data = [base_page_migration, four_thread_concur_base_page, four_thread_concur_exchange_base_page]
+    base_page_labels = ["Base Page Migration", "4-Thread Concur Migrate", "4-Thread Concur Exchange"]
 
     for j in range(0, len(base_y_data)):
         for i in page_count:
@@ -91,8 +90,8 @@ def main():
     fig, axs = plt.subplots(nrows=2)
     axs[0].grid(True)
     axs[1].grid(True)
-    sns.lineplot(data=thp_data, x="pagecount", y="bandwidth", hue="migration_type", ax=axs[0]).set(title="Figure 10: Throughput comparison of concurrent page migration(4 threads)", xlabel="Number of Pages", ylabel="Throughput(GB/GCycles)")
-    sns.lineplot(data=base_page_data, x="pagecount", y="bandwidth", hue="migration_type", ax=axs[1]).set(title="Figure 10: Throughput comparison of concurrent page migration(4 threads)", xlabel="Number of Pages", ylabel="Throughput(GB/GCycles)")
+    sns.lineplot(data=thp_data, x="pagecount", y="bandwidth", hue="migration_type", ax=axs[0]).set(title="Figure 10: Throughput comparison of exchange THP page migration(4 threads)", xlabel="Number of Pages", ylabel="Throughput(GB/GCycles)")
+    sns.lineplot(data=base_page_data, x="pagecount", y="bandwidth", hue="migration_type", ax=axs[1]).set(title="Figure 10: Throughput comparison of exchange base page migration(4 threads)", xlabel="Number of Pages", ylabel="Throughput(GB/GCycles)")
     plt.tight_layout()
     plt.savefig('Plot_4.png')
 
