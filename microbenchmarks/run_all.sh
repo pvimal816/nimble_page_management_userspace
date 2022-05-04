52bd30b39834ba0b28cbc06dc8a4f3e112edba10
Vimal Patel <vimalm@iisc.ac.in>
2022-05-04 10:30:48 +0530

    microbenchmark performance data for dram to dram

--- microbenchmarks/run_all.sh
+++ microbenchmarks/run_all.sh
@@ -11,51 +11,48 @@ DESTINATION_NODES=(3 9 3 9)
 CONFIGURATION_NAMES=("dram to dram" "dram to pmem" "pmem to dram" "pmem to pmem")
 
 for i in `seq 0 3`; do
-    # export SOURCE_NODE=${SOURCE_NODES[$i]}
-    # export SOURCE_CPU_NODE=${SOURCE_CPU_NODES[$i]}
-    # export DESTINATION_NODE=${DESTINATION_NODES[$i]}
+    export SOURCE_NODE=${SOURCE_NODES[$i]}
+    export SOURCE_CPU_NODE=${SOURCE_CPU_NODES[$i]}
+    export DESTINATION_NODE=${DESTINATION_NODES[$i]}
 
-    # # run concurrent page migration benchmarks
-    # cd concurrent_page_migration;
-    # make non_thp_move_pages;
-    # make thp_move_pages;
-    # echo "============executing concurrent_page_migration/run_non_thp_test.sh================"
-    # ./run_non_thp_test.sh
-    # echo "============executing concurrent_page_migration/run_thp_test.sh===================="
-    # ./run_thp_test.sh
-    # cd ..
+    # run concurrent page migration benchmarks
+    cd concurrent_page_migration;
+    make non_thp_move_pages;
+    make thp_move_pages;
+    echo "============executing concurrent_page_migration/run_non_thp_test.sh================"
+    ./run_non_thp_test.sh
+    echo "============executing concurrent_page_migration/run_thp_test.sh===================="
+    ./run_thp_test.sh
+    cd ..
 
-    # # run exchange page migration benchmark
-    # cd exchange_page_migration
-    # make non_thp_move_pages;
-    # make thp_move_pages;
-    # echo "============executing exchange_page_migration/run_non_thp_test.sh================"
-    # ./run_non_thp_test.sh
-    # echo "============executing exchange_page_migration/run_thp_test.sh===================="
-    # ./run_thp_test.sh
-    # cd ..
+    # run exchange page migration benchmark
+    cd exchange_page_migration
+    make non_thp_move_pages;
+    make thp_move_pages;
+    echo "============executing exchange_page_migration/run_non_thp_test.sh================"
+    ./run_non_thp_test.sh
+    echo "============executing exchange_page_migration/run_thp_test.sh===================="
+    ./run_thp_test.sh
+    cd ..
 
-    # # run parallel and native thp page migration benchmark
-    # cd thp_page_migration_and_parallel
-    # make non_thp_move_pages;
-    # make thp_move_pages;
-    # echo "============executing thp_page_migration_and_parallel/run_non_thp_test.sh================"
-    # ./run_non_thp_test.sh
-    # echo "============executing thp_page_migration_and_parallel/run_non_thp_2mb_page_test.sh================"
-    # ./run_non_thp_2mb_page_test.sh
-    # echo "============executing thp_page_migration_and_parallel/run_split_thp_test.sh================"
-    # ./run_split_thp_test.sh
-    # echo "============executing thp_page_migration_and_parallel/run_thp_test.sh================"
-    # ./run_thp_test.sh
-    # cd ..
+    # run parallel and native thp page migration benchmark
+    cd thp_page_migration_and_parallel
+    make non_thp_move_pages;
+    make thp_move_pages;
+    echo "============executing thp_page_migration_and_parallel/run_non_thp_test.sh================"
+    ./run_non_thp_test.sh
+    echo "============executing thp_page_migration_and_parallel/run_non_thp_2mb_page_test.sh================"
+    ./run_non_thp_2mb_page_test.sh
+    echo "============executing thp_page_migration_and_parallel/run_split_thp_test.sh================"
+    ./run_split_thp_test.sh
+    echo "============executing thp_page_migration_and_parallel/run_thp_test.sh================"
+    ./run_thp_test.sh
+    cd ..
     
     # push this configuration data to repository
-    echo "user" | sudo -S su -c 'echo user:$USER' vimal
     echo "user" | sudo -S su -c 'git add .' vimal
     echo "user" | sudo -S su -c "git commit -m 'microbenchmark performance data for ${CONFIGURATION_NAMES[$i]}'" vimal
-    # git add .
-    echo "microbenchmark performance data for ${CONFIGURATION_NAMES[$i]}"
-    git push
+    echo "user" | sudo -S su -c "git push" vimal
 done
 
 sudo sysctl kernel.numa_balancing=1
